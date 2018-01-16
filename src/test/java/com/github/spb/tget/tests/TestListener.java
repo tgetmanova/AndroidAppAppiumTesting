b5a0e90f8da75f0dfaf80bc6736ce193f5ba1897
tgetmanova <tatyana.getmanova@gmail.com>
2018-01-16 23:39:03 +0300

    Replace ITestListener interface with TestListeAdapter impl

--- src/test/java/com/github/spb/tget/tests/TestListener.java
+++ src/test/java/com/github/spb/tget/tests/TestListener.java
@@ -6,46 +6,15 @@ import io.qameta.allure.Attachment;
 
 import org.openqa.selenium.OutputType;
 
-import org.testng.ITestContext;
-import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
 
-public class TestListener extends BaseTest implements ITestListener {
-
-    @Override
-    public void onTestStart(ITestResult iTestResult) {
-
-    }
-
-    @Override
-    public void onTestSuccess(ITestResult iTestResult) {
-
-    }
+public class TestListener extends TestListenerAdapter {
 
     @Override
     public void onTestFailure(ITestResult result) {
         Object currentClass = result.getInstance();
         saveScreenshotAsPng(((BaseTest) currentClass).driver);
-    }
-
-    @Override
-    public void onTestSkipped(ITestResult iTestResult) {
-
-    }
-
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-
-    }
-
-    @Override
-    public void onStart(ITestContext iTestContext) {
-
-    }
-
-    @Override
-    public void onFinish(ITestContext iTestContext) {
-
     }
 
     @Attachment(value = "Page screenshot", type = "image/png")
